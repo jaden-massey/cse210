a0d0a5683c7f80133532c745d6610507cb6daaf4
jaden-massey <122401895+jaden-massey@users.noreply.github.com>
2024-03-09 22:47:49 -0700

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.Write($"{number}");
+
+        int guess = 0;
+        bool guessed = false;
+
+        while (guessed == false)
+        { 
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+            guessed = true;
+            Console.WriteLine("You guessed the number!");
+            }
+        }
     }
 }
