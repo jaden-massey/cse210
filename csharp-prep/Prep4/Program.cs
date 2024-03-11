fba1b1a57009e720ab6bc64d4c8a7fe16e4231cb
jaden-massey <122401895+jaden-massey@users.noreply.github.com>
2024-03-11 01:20:23 -0600

    finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        // initialize variables & lists
+        int input = 0;
+        int sum = 0;
+
+        List<int> numbers = new List<int>();
+
+        // generate list, add user inputs until 0 is enetered
+        do
+        {
+            Console.Write("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }   
+
+        } while (input != 0);
+
+        // calc sum
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        // calc average
+        float average =  (float)sum / (numbers.Count);
+
+
+        // get max
+        int max = numbers.Max();
+
+        // print results to user
+        Console.WriteLine($@"
+The sum is: {sum}
+The average is: {average}
+The largest number is: {max}");
+
     }
 }
