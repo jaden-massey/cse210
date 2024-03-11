2f17a6db8574f589eda0c2c6154ef51b1b60de31
jaden-massey <122401895+jaden-massey@users.noreply.github.com>
2024-03-11 01:42:41 -0600

    finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // define functions
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+        
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static double SquareNumber(int number)
+        {
+            return Math.Pow(number, 2);
+        }
+
+        static void DisplayResult()
+        {
+            Console.WriteLine($"{PromptUserName()}, the square of your number is {SquareNumber(PromptUserNumber())}");
+        }
+
+        DisplayWelcome();
+        DisplayResult();
     }
 }
